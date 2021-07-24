e29005cd11ec0f04d7eaee17638ca3d6f2a609a6
tsekeres <twsekeres@gmail.com>
2021-07-24 12:36:07 -0500

    Add project files.

---
+++ Urban Planner/Building.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urban_Planner
+{
+    class Building
+    {
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public int Volume { get; private set; }
+
+        string _designer;
+        DateTime _dateConstructed;
+        string _address;
+        string _owner;
+
+        public Building()
+        {
+            _designer = "Tad Sekeres";
+        }
+
+    }
+}
