f970bd6c88a19e3d173bc2181b9a31180f021ec5
tsekeres <twsekeres@gmail.com>
2021-07-31 08:54:17 -0500

    Finshed up th eproject

--- Urban Planner/Building.cs
+++ Urban Planner/Building.cs
@@ -4,23 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Urban_Planner
+namespace UrbanPlanner
 {
     class Building
     {
         public int Stories { get; set; }
         public double Width { get; set; }
         public double Depth { get; set; }
-        public int Volume { get; private set; }
+        public int Volume => (int)(Width * Depth * (3 * Stories));
 
-        string _designer;
-        DateTime _dateConstructed;
-        string _address;
-        string _owner;
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
 
-        public Building()
+        
+
+        public Building(string address)
         {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
             _designer = "Tad Sekeres";
+        }
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+            Console.WriteLine($"{_address}--------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
         }
 
     }
--- Urban Planner/Program.cs
+++ Urban Planner/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace Urban_Planner
+namespace UrbanPlanner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var BuildingOne = new Building("512 8th Avenue");
+            BuildingOne.Depth = 120;
+            BuildingOne.Stories = 8;
+            BuildingOne.Width = 50;
+
+            BuildingOne.Construct();
+            BuildingOne.Purchase("Bill Purcell");
+
+            var BuildingTwo = new Building("1145 6th Street");
+            BuildingTwo.Depth = 100;
+            BuildingTwo.Stories = 8;
+            BuildingTwo.Width = 40;
+
+            BuildingTwo.Construct();
+            BuildingTwo.Purchase("Ron Nollner");
+
+            var BuildingThree = new Building("1600 Pennsylvania");
+            BuildingThree.Depth = 90;
+            BuildingThree.Stories = 8;
+            BuildingThree.Width = 50;
+
+            BuildingThree.Construct();
+            BuildingThree.Purchase("Jill Petty");
+
+            var BuildingFour = new Building("810 Streety Street");
+            BuildingFour.Depth = 130;
+            BuildingFour.Stories = 8;
+            BuildingFour.Width =25;
+
+            BuildingFour.Construct();
+            BuildingFour.Purchase("Don Rickles");
         }
     }
 }
